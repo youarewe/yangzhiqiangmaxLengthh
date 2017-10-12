4c39fcd77b2475919411fb5285061848d561ae00
youarewe <binglintiandi@163.com>
2017-10-12 22:01:29 +0800

    first commit

---
+++ BuChongfu/src/Main.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(maxLength("bbb"));
+
+	}
+	@Test
+	public void test1(){
+		
+		int res = maxLength("bbb");
+		assertEquals(1, res);
+	}
+	
+	public static int maxLength(String str){  
+        if(str==null||str.equals("")){  
+            return 0;  
+        }  
+        Set<Character> set=new HashSet<Character>();  
+        int maxLength=0;  
+        int pre=0;  
+        int after=0;  
+        while(after<str.length()){  
+            if(!set.contains(str.charAt(after))){  
+                set.add(str.charAt(after));  
+                after++;  
+            }else{  
+                set.clear();  
+                //System.out.println("pre:  "+pre+"  after:  "+after);  
+                if((after-pre)>maxLength){  
+                    maxLength=after-pre;  
+                }  
+                pre++;  
+                after=pre;  
+            }  
+        }  
+          
+        if((after-pre)>maxLength){  
+            maxLength=after-pre;  
+        }  
+        return maxLength;  
+    }  
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # yangzhiqiangmaxLengthh
 # yangzhiqiangmaxLengthh
+# yangzhiqiangmaxLengthh
